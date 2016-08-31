72a82fcb2a25bb9df7f49f0cb73b6d06b2d2f1c4
liyubin <lixin58688@163.com>
2016-08-31 10:55:45 +0800

    modify: Day1214

--- src/Day1214/InnerTest.java
+++ src/Day1214/InnerTest.java
@@ -8,23 +8,40 @@ import java.awt.*;
 public class InnerTest {
     public static void main(String args[]){
         Outer out1 = new Outer();  //外部类实例化对象
-        out1.fun();
+        //out1.fun();
 
-        Outer.Inner in = new Outer.Inner(); //外部可直接调用静态内部类
+        Outer.StaticInner static_in = new Outer.StaticInner(); //外部可直接调用静态内部类
+        static_in.print();
+        
+        Outer.Inner in=new Outer().new Inner();
         in.print();
+        
     }
 }
 
 class Outer{
     private static String info = "Hello World!";
-
-    static class Inner {
+    
+    //常规内部类
+    public class Inner {
         public void print(){
-            System.out.println(info);
+            System.out.println(info+"<>");
+        }
+    }
+    //静态内部类
+    public static class StaticInner {
+        public void print(){
+            System.out.println(info+"<static>");
         }
     }
 
     public void fun(){
-        new Inner().print();
+    	final String info2="info2";
+    	class Inner2{
+    		public void print2(){
+                System.out.println(info2);
+            }
+    	}
+        new StaticInner().print();
     }
 }
