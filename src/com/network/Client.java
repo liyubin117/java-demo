e6fba97c2bc672bf5ce7d64f3fcab0b8e02a243d
liyubin <lixin58688@163.com>
2016-10-17 09:36:48 +0800

    modify client.java

--- src/com/network/Client.java
+++ src/com/network/Client.java
@@ -4,17 +4,22 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.net.SocketAddress;
 import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
 
 public class Client {
-	private static final int TIMEOUT=1;
+	private static final int TIMEOUT=5;
 	
 	public static void main(String[] args) throws IOException{
 		while(true){
 			try{
-				Socket s=new Socket(InetAddress.getLocalHost(),10888);
+				Socket s=new Socket();
+				s.connect(new InetSocketAddress(InetAddress.getLocalHost(),10888), TIMEOUT*1000);
+				/*Socket s=new Socket(InetAddress.getLocalHost(),10888);
+				s.setSoTimeout(TIMEOUT*1000);*/
 				s.setSoTimeout(TIMEOUT*1000);
 				BufferedReader br=new BufferedReader(new InputStreamReader(s.getInputStream()));
 				String line=br.readLine();
