22740601f7a6d6ca5d6a56084b2a1a903ca69def
code159 <409901403@qq.com>
2016-11-21 21:17:42 +0800

    modify TestMyJDBCAPI.java

--- src/com/sql/TestMyJDBCAPI.java
+++ src/com/sql/TestMyJDBCAPI.java
@@ -13,28 +13,34 @@ public class TestMyJDBCAPI {
 				"root", 
 				"mysql");
 		
-		DML insert=new DML(conn,"t1","id,name","8,\"li8\"");
-		insert.insert();
+		DML insert=null;
+		DML update=null;
+		Select select=null;
 		
-//		DML delete=new DML(conn,"t1",null,"where id=8");
-//		delete.delete();
-		
-		DML update=new DML(conn,"t1",null,"set name=\"hi\" where id=8");
-		update.update();
-		
-		Select select=new Select(conn,"t1","id,name","");
-		select.execute();
-		
-		
-		insert=new DML(conn,"t2","id,name,addr"," 1,\"liyubin\",\"hangzhou\" ");
-		insert.insert();
-		
-		select=new Select(conn,"t2","*","");
-		select.execute();
-		
-		
-		if(conn!=null){
-			conn.close();
-		}
+		try{
+			insert=new DML(conn,"t1","id,name","8,\"li8\"");
+			insert.insert();
+			
+//			DML delete=new DML(conn,"t1",null,"where id=8");
+//			delete.delete();
+			
+			update=new DML(conn,"t1",null,"set name=\"hi\" where id=8");
+			update.update();
+			
+			select=new Select(conn,"t1","id,name","");
+			select.execute();
+			
+			
+			insert=new DML(conn,"t2","id,name,addr"," 1,\"liyubin\",\"hangzhou\" ");
+			insert.insert();
+			
+			select=new Select(conn,"t2","*","");
+			select.execute();
+		}finally{
+			if(conn!=null){
+				conn.close();
+			}
+		}		
+
 	}
 }
