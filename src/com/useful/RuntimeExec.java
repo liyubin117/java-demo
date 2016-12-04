71d2ef8c513e89d9f08ae8c1a57ac7d9be169719
liyubin <lixin58688@163.com>
2016-12-04 17:50:39 +0800

    add com.useful about Runtime

---
+++ src/com/useful/RuntimeExec.java
@@ -0,0 +1,25 @@
+package com.useful;
+
+import java.io.IOException;
+
+public class RuntimeExec {
+	public static void main(String[] args){
+		Runtime run=Runtime.getRuntime();
+
+		Process p=null;
+		
+		try {
+			p=run.exec("notepad.exe");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try{
+			Thread.sleep(3000);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		p.destroy();
+	}
+}
