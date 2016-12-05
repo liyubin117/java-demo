c63ca0446d93ffd33e37e241ea44ab70c6d7a863
code159 <409901403@qq.com>
2016-12-05 23:23:14 +0800

    add SystemDemo.java

---
+++ src/com/useful/SystemDemo.java
@@ -0,0 +1,14 @@
+package com.useful;
+
+public class SystemDemo {
+
+	public static void main(String[] args) {
+		//取得当前系统的所有属性
+		System.getProperties().list(System.out);
+		
+		System.out.println("当前字符集："+System.getProperty("file.encoding")
+		+"\n当前目录："+System.getProperty("user.dir")
+		);
+	}
+
+}
