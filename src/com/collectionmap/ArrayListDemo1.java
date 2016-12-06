b59fe91296f9ebb75b704623c810516bd4291500
code159 <409901403@qq.com>
2016-12-06 21:59:25 +0800

    增加关于ArrayList使用toArray get遍历

--- src/com/collectionmap/ArrayListDemo1.java
+++ src/com/collectionmap/ArrayListDemo1.java
@@ -9,16 +9,32 @@ public class ArrayListDemo1 {
 	public static void main(String[] args) {
 		List<String> ls=new ArrayList<String>();
 		ls.add("hello");
-		ls.add("hello");
+		ls.add(",");
 		ls.add("liyubin");
 		Iterator i=ls.iterator();
+		System.out.print("Iterator遍历:");
 		while(i.hasNext()){
-			System.out.println(i.next());
+			System.out.print(i.next()+" ");
 		}
+		System.out.println();
+		System.out.print("for each遍历:");
 		for(String s:ls){
-			System.out.println(s);
+			System.out.print(s+" ");
 		}
+		System.out.println();
+
+		System.out.print("toString遍历:");
 		System.out.println(ls);
+		
+		System.out.println(ls.get(2));
+		
+		System.out.print("toArray get遍历:");
+		Object[] obj=ls.toArray();
+		for(int n=0;n<obj.length;n++){
+			System.out.print(obj[n]+" ");
+		}
+		System.out.println();
+
 	}
 
 }
