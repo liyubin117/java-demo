037a4df180c3922e01cbb21c7fa2704f96650a1f
liyubin <lixin58688@163.com>
2016-12-08 20:04:15 +0800

    增加Cloneable接口相关内容

---
+++ src/com/useful/CloneableDemo1.java
@@ -0,0 +1,30 @@
+package com.useful;
+
+class Person implements Cloneable{
+	private String name;
+	public Person(String name){
+		this.name=name;
+	}
+	public void setName(String name){
+		this.name=name;
+	}
+	public String toString(){
+		return "Person:"+this.name;
+	}
+	@Override
+	public Object clone() throws CloneNotSupportedException{
+		return super.clone();
+	}
+}
+
+public class CloneableDemo1
+{
+	public static void main(String[] args) throws CloneNotSupportedException{
+		Person p1=new Person("张三");
+		Person p2=(Person)p1.clone();
+		p2.setName("李四");
+		System.out.println(p1);
+		System.out.println(p2);
+
+	}
+};
