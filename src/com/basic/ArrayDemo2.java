05f52f7bce93928dedf44a923aff5eef06648cda
liyubin <lixin58688@163.com>
2016-12-15 10:51:05 +0800

    调整结构

---
+++ src/com/basic/ArrayDemo2.java
@@ -0,0 +1,86 @@
+package com.basic;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Administrator on 2016-2-5.
+ */
+public class ArrayDemo2 {
+    public static void main(String[] args) {
+        int[] s = {1, 3, 120, 8, 83,24,56,78}; //静态初始化数组
+        fun(s);
+        print(s);
+
+        System.out.println();
+        ArraySort.sort(s);
+        ArraySort.print(s);
+        //OR
+        System.out.println();
+        int[] s2 = {1, 7, 120, 8, 83,24,56,78};
+        java.util.Arrays.sort(s2);
+        ArraySort.print(s2);
+
+        //copy
+        System.out.println();
+        int[] s3 = {1,2,3,4,5};
+        int[] d = {11,22,33,44,55};
+        ArrayCopy.copy(s3, 2, d, 3, 3);
+        print(d);
+        //OR
+        System.out.println();
+        int[] s4 = {1,2,3,4,5};
+        int[] d2 = {11,22,33,44,55};
+        System.arraycopy(s4,2,d2,3,2);
+        print(d2);
+
+        System.out.println();
+        for (int i=1;i<10;i++){
+            for(int j=1;j<i+1;j++){
+                System.out.print(i+"*"+j+"="+(i*j)+"\t");
+            }
+            System.out.println();
+        }
+
+    }
+        public static void fun(int[] x){
+            x[1]=7;
+        }
+        public static void print(int[] a){
+            for(int i=0;i<a.length;i++){
+                System.out.print(a[i]+" ");
+            }
+        }
+}
+
+
+class ArraySort{
+    public static void sort(int[] temp){
+        for (int i=1;i<temp.length;i++){
+            for(int j=0;j<temp.length;j++){
+                if(temp[i]<temp[j]){
+                    int x = temp[i];
+                    temp[i] = temp[j];
+                    temp[j] = x;
+                }
+            }
+        }
+    }
+    public static void print(int[] temp){
+        for (int i=0;i<temp.length;i++){
+            System.out.print(temp[i]+"\t");
+        }
+    }
+}
+
+class ArrayCopy{
+    public static void copy(int[] s,int a,int[] d,int b,int length){
+        if((a+length)>(s.length)){
+            System.out.println("out of range!");
+        }else{
+            for(int i=0;i<length;i++){
+                d[b+i] = s[a+i];
+            }
+        }
+
+    }
+}
---
+++ src/com/basic/StringDemo1.java
@@ -0,0 +1,40 @@
+package com.basic;
+
+/**
+ * Created by Administrator on 2015-9-16.
+ * String类型
+ */
+public class StringDemo1 {
+    public static void main(String args[]){
+        //提取子串,每一个双引号括起来的字串都是String类型的一个实例，substring的第二个参数是第一个不想提取的字符位置
+        System.out.println("Hello".substring(0,2));
+        //对于String型对象进行相等性测试时，只能使用equals函数，==比较的是两个String型对象的内存位置是否相同。
+        // 只有当两对象指向的是字符串常量时，==才起作用，而当使用+、substring等操作后，即使一样其位置也可能不一样了。为了保证一致性，使用equals函数判断相等性
+        String s1 = "hello1";
+        String s2 = "hello1";
+        if (s1 == s2){
+            System.out.println("==相等");
+        }
+        if (s1.substring(0,5) == s2.substring(0,5)){
+            System.out.println("==相等");
+        }
+        else {
+            System.out.println("substring运算后内存位置不同");
+        }
+        if (s1.substring(0,5).equals(s2.substring(0,5))){
+            System.out.println("equals相等");
+        }
+
+        //代码点与代码单元
+        //length方法返回采用UTF-16编码表示的给定字符串所需要的代码单元数量
+        //codePoint返回代码点数量
+        String s3 = "Hello";
+        System.out.println(s3.length());
+        System.out.println(s3.codePointCount(0, s3.length()));
+        //若需要用许多小段的字符串构建一个字符串，为保证效率，需要使用StringBuilder
+        StringBuilder builder1 = new StringBuilder();
+        builder1.append("!aaaa");
+        builder1.append('b');
+        System.out.println(builder1);
+    }
+}
