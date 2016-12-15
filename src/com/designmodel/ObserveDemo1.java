4d8b2cbd5be944c9d502058ade327eb86c7b1cf6
liyubin <lixin58688@163.com>
2016-12-15 10:10:43 +0800

    新增Pattern、Matcher正则和String正则

--- src/com/designmodel/ObserveDemo1.java
+++ src/com/designmodel/ObserveDemo1.java
@@ -4,8 +4,10 @@ import java.util.Observable;
 import java.util.Observer;
 
 class House extends Observable{
+	private String houseName;
 	private double price;
-	public House(double d){
+	public House(String h,double d){
+		this.houseName=h;
 		this.price=d;
 	}
 	public double getPrice(){
@@ -16,8 +18,13 @@ class House extends Observable{
 		super.notifyObservers(d); //通知观察者价格变为d
 		this.price=d;
 	}
+	public synchronized void setHouseName(String s){
+		super.setChanged();  //设置变化点
+		super.notifyObservers(s); //通知观察者房屋名字变为d
+		this.houseName=s;
+	}
 	public String toString(){
-		return "房子价格为："+this.price;
+		return this.houseName+"房子价格为："+getPrice();
 	}
 }
 
@@ -31,7 +38,10 @@ class HousePriceObserver implements Observer{
 	public void update(Observable o, Object arg) {
 		if(arg instanceof Double){
 			this.price=((Double)arg).doubleValue();
-			System.out.println(this.name+"观察到的价格更改为："+this.price+o);
+			System.out.println(this.name+"观察到的价格更改为："+this.price+" "+o);
+		}
+		if(arg instanceof String){
+			System.out.println(this.name+"观察到的房屋名字更改为："+(String)arg+" "+o);
 		}
 	}	
 }
@@ -39,22 +49,24 @@ class HousePriceObserver implements Observer{
 public class ObserveDemo1 {
 
 	public static void main(String[] args) {
-		House h=new House(10000);
-		House h2=new House(50000);
+		House h1=new House("h1",10000);
+		House h2=new House("h2",50000);
 		
 		HousePriceObserver hpo1=new HousePriceObserver("li");
 		HousePriceObserver hpo2=new HousePriceObserver("ye");
 		HousePriceObserver hpo3=new HousePriceObserver("aa");
-		h.addObserver(hpo1);
-		h.addObserver(hpo2);
-		h.addObserver(hpo3);
+		h1.addObserver(hpo1);
+		h1.addObserver(hpo2);
+		h1.addObserver(hpo3);
 		
 		h2.addObserver(hpo2);
 		
-		h.setPrice(8000);
+		h1.setPrice(8000);
 		h2.setPrice(5000);
-		System.out.println(h);
+		System.out.println(h1);
 		System.out.println(h2);
+		
+		h1.setHouseName("h3");
 	}
 
 }
---
+++ src/com/useful/RegexDemo1.java
@@ -0,0 +1,63 @@
+package com.useful;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;;
+
+class Regex{
+	private Pattern p;
+	private Matcher m;
+	public Regex(String reg,String s){
+		this.p=Pattern.compile(reg);
+		this.m=p.matcher(s);
+	}
+	public Regex(String reg){
+		this.p=Pattern.compile(reg);
+	}
+	
+	public boolean getResult(){
+		return m.matches();
+	}
+	public String replace(String s){
+		return m.replaceAll(s);
+	}
+	public String[] getSplit(String s){
+		return p.split(s);
+	}
+}
+
+public class RegexDemo1 {
+	public static void main(String[] args){
+		Regex r=new Regex("[0-9]+","123890");
+		if(r.getResult()){
+			System.out.println("由数字组成");
+		}else{
+			System.out.println("不全是数字");
+		}
+		
+		r=new Regex("\\d{4}-\\d{2}-\\d{2}","1988-09-10");
+		if(r.getResult()){
+			System.out.println("日期格式符合要求");
+		}else{
+			System.out.println("日期格式不符合要求");
+		}
+		
+		r=new Regex("\\d","12a89b");
+		System.out.println(r.replace("_"));
+		
+		r=new Regex("\\d+");
+		for(String s:r.getSplit("abc123def456")){
+			System.out.print(s+"\t");
+		}
+		System.out.println();
+		
+		//使用String类的正则
+		String s="aa123bb456cc";
+		if(s.matches("\\d")){
+			System.out.println("是数字");
+		}
+		System.out.println(s.replaceAll("\\d","_"));
+		String[] ss=s.split("\\d+");
+		for(int i=0;i<ss.length;i++){
+			System.out.print(ss[i]+"\t");
+		}
+	}
+}
