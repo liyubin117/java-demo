02e8dd877f3d6e23d5db359704ad9e50fece8b10
liyubin <lixin58688@163.com>
2016-12-22 13:51:38 +0800

    新增BufferedReader、CharSet、管道IO、PrintStream、System IO相关实例

---
+++ src/com/io/BufferedReaderDemo1.java
@@ -0,0 +1,20 @@
+package com.io;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BufferedReaderDemo1 {
+
+	public static void main(String[] args) {
+		BufferedReader buf=new BufferedReader(new InputStreamReader(System.in));
+		String str=null;
+		System.out.print("请输入内容：");
+		try{
+			str=buf.readLine();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		System.out.println("输出内容："+str);
+	}
+
+}
