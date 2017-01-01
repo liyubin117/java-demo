47cd51021ed07eae7e841e5825412cc8e504290b
liyubin <lixin58688@163.com>
2017-01-01 11:16:06 +0800

    新增反射的应用：取得类结构、调用方法、获取或设置属性、创建对象

---
+++ src/com/reflect/ReflectArrayDemo1.java
@@ -0,0 +1,16 @@
+package com.reflect;
+
+import java.lang.reflect.Array;
+
+public class ReflectArrayDemo1 {
+
+	public static void main(String[] args) {
+		int[] arr={1,2,3};
+		Class<?> c=arr.getClass().getComponentType();
+		System.out.println("类型："+c.getName());
+		System.out.println("长度："+Array.getLength(arr));
+		System.out.println("第2个值是："+Array.get(arr,1));
+
+	}
+
+}
