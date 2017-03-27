fe25b47aeb4f2ea251c3333bab62602d6a606fc5
liyubin <lixin58688@163.com>
2017-03-27 19:47:52 +0800

    新增策略模式

---
+++ src/com/designmodel/StrategyDemo.java
@@ -0,0 +1,54 @@
+package com.designmodel;
+//策略模式
+public class StrategyDemo {
+	public static void main(String[] args) {
+		Context context = new Context(new OperationAdd());
+		System.out.println("10 + 5 = " + context.executeStrategy(10, 5));
+
+		context = new Context(new OperationSubstract());
+		System.out.println("10 - 5 = " + context.executeStrategy(10, 5));
+
+		context = new Context(new OperationMultiply());
+		System.out.println("10 * 5 = " + context.executeStrategy(10, 5));
+	}
+}
+
+// 接口
+interface Strategy {
+	public int doOperation(int num1, int num2);
+}
+
+// 实现接口的实体类
+class OperationAdd implements Strategy {
+	@Override
+	public int doOperation(int num1, int num2) {
+		return num1 + num2;
+	}
+}
+
+class OperationSubstract implements Strategy {
+	@Override
+	public int doOperation(int num1, int num2) {
+		return num1 - num2;
+	}
+}
+
+class OperationMultiply implements Strategy {
+	@Override
+	public int doOperation(int num1, int num2) {
+		return num1 * num2;
+	}
+}
+
+// 并装、选择策略的Context类
+class Context {
+	private Strategy strategy;
+
+	public Context(Strategy strategy) {
+		this.strategy = strategy;
+	}
+
+	public int executeStrategy(int num1, int num2) {
+		return strategy.doOperation(num1, num2);
+	}
+}
