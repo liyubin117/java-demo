e257b19e7196d831b8123f0242c1ba0dbd0cc543
liyubin <lixin58688@163.com>
2017-04-26 17:36:52 +0800

    新增return例子

--- src/com/collectionmap/HashMapDemo.java
+++ src/com/collectionmap/HashMapDemo.java
@@ -3,6 +3,7 @@ package com.collectionmap;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
+import java.util.Set;
 
 public class HashMapDemo {
 	public static void main(String[] args) {
@@ -25,9 +26,26 @@ public class HashMapDemo {
 		
 		//构造方式
 		//public HashMap(Map<? extends K, ? extends V> m)
+		System.out.println("----构造方法--------");
 		Map<Number, Number> map = new HashMap<>(countMap);
 		for(Map.Entry<Number, Number> kv : map.entrySet()){
 		    System.out.println(kv.getKey()+","+kv.getValue());
 		}
+		
+		System.out.println("-----Map.Entry-----");
+		Set<Map.Entry<Number, Number>> entrys=map.entrySet();
+		for(Map.Entry<Number, Number> entry:entrys){
+			if(entry.getKey().equals(1)){
+				continue;
+			}
+		    System.out.println(entry.getKey()+","+entry.getValue());
+		}
+		//keySet、values、entrySet返回视图
+		map.values().clear();
+		System.out.print("clear后：");
+		for(Map.Entry<Number, Number> kv : map.entrySet()){
+		    System.out.println(kv.getKey()+","+kv.getValue());
+		}
+		
 	}
 }
