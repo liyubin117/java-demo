9030202604eaaaef54014ebe5566f7a00f069495
liyubin <lixin58688@163.com>
2017-05-18 13:29:38 +0800

    新增Collections适配器、装饰器

--- src/com/generic/DynamicArray.java
+++ src/com/generic/DynamicArray.java
@@ -63,6 +63,17 @@ public class DynamicArray<E> {
 	        dest.add(get(i));
 	    }
 	}
+	
+	//如果返回值依赖于类型参数，不能用通配符
+	public static <T extends Comparable<T>> T max(DynamicArray<T> arr){
+	    T max = arr.get(0);
+	    for(int i=1; i<arr.size(); i++){
+	        if(arr.get(i).compareTo(max)>0){
+	            max = arr.get(i);
+	        }
+	    }
+	    return max;
+	}
 	/**
 	 * @param args
 	 */
@@ -78,6 +89,8 @@ public class DynamicArray<E> {
 		for(int i=0;i<numbers.size;i++){
 			System.out.print(numbers.get(i)+" ");
 		}
+		System.out.println();
+		System.out.println(max(ints));
 
 	}
 
