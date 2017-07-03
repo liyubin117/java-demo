4bfe4977e1ed870f75bd4e3359178f4aae52a264
liyubin <lixin58688@163.com>
2017-07-03 17:36:09 +0800

    新增定时任务的两种使用方法：Timer/TimerTask、ScheduledExecutorService

---
+++ src/com/threads/timer/BasicTimer.java
@@ -0,0 +1,22 @@
+package com.threads.timer;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class BasicTimer {
+    static class DelayTask extends TimerTask {
+        
+        @Override
+        public void run() {
+            System.out.println("delayed task");
+        }
+    }
+    
+    public static void main(String[] args) throws InterruptedException {
+    	//延时1秒后执行
+        Timer timer = new Timer();
+        timer.schedule(new DelayTask(), 1000);
+        Thread.sleep(2000);
+        timer.cancel();
+    }
+}
