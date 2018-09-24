b7b8f13412a1824e7fa9bc7d189258d8be097570
liyubin <lixin58688@163.com>
2018-09-24 18:48:38 +0800

    新增日志框架slf4j、调度框架quartz示例

---
+++ src/com/useful/Slf4jDemo.java
@@ -0,0 +1,19 @@
+package com.useful;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Slf4jDemo {
+    private static final Logger logger = LoggerFactory.getLogger(Slf4jDemo.class);
+
+    public static void main(String[] args) {
+        logger.info("Current Time: {}", System.currentTimeMillis());
+        logger.info("Current Time: " + System.currentTimeMillis());
+        logger.info("Current Time: {}", System.currentTimeMillis());
+        logger.trace("trace log");
+        logger.warn("warn log");
+        logger.debug("debug log");
+        logger.info("info log");
+        logger.error("error log");
+    }
+}
