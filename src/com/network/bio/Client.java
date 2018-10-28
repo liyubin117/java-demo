6e2d07ad8c8abfef62002f37a449ae45a5dee849
liyubin <lixin58688@163.com>
2018-10-28 22:50:55 +0800

    修改bio代码

---
+++ src/com/network/bio/Client.java
@@ -0,0 +1,41 @@
+package com.network.bio;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+//BIO客户端，可循环设置端口号
+public class Client {
+	private static final int TIMEOUT=5;
+	private static int port=0;
+	
+	public static void main(String[] args) throws IOException{
+		while(true){
+			System.out.println("请输入端口号：");
+			Scanner scanner=new Scanner(System.in);
+			port=scanner.nextInt();
+			try{
+				Socket s=new Socket();
+				s.connect(new InetSocketAddress(InetAddress.getLocalHost(),port), TIMEOUT*1000);
+				/*Socket s=new Socket(InetAddress.getLocalHost(),10888);
+				s.setSoTimeout(TIMEOUT*1000);*/
+				s.setSoTimeout(TIMEOUT*1000);
+				BufferedReader br=new BufferedReader(new InputStreamReader(s.getInputStream()));
+				String line=br.readLine();
+				System.out.println("来自服务器的数据："+line);
+				
+				br.close();
+				s.close();
+			}catch(SocketTimeoutException e){
+				System.out.println("超过"+TIMEOUT+"s，停止接收");
+			}
+		}
+
+	}
+}
