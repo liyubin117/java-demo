93f877fca8140b1cc5c099f70ad5e96807027d54
YuBin Li <lixin58688@163.com>
2020-10-27 19:22:29 +0800

    add cache false share

--- src/com/jdksource/InstanceLayout.java
+++ src/com/jdksource/InstanceLayout.java
@@ -6,5 +6,9 @@ public class InstanceLayout {
     public static void main(String[] args) {
         Object o = new Object();
         System.out.println(ClassLayout.parseInstance(o).toPrintable());
+
+        synchronized (o){
+            System.out.println(ClassLayout.parseInstance(o).toPrintable());
+        }
     }
 }
--- src/com/threads/CountDownLatchDemo.java
+++ src/com/threads/CountDownLatchDemo.java
@@ -9,47 +9,38 @@ public class CountDownLatchDemo {
     public static void main(String[] args) {
         final CountDownLatch latch = new CountDownLatch(2);
 
-        Thread s1 = new Thread(){
-          @Override
-          public void run(){
-              System.out.println("线程1启动...，等待线程2、3完成后通知其");
-              try {
-                  latch.await();
-              } catch (InterruptedException e) {
-                  e.printStackTrace();
-              }
-              System.out.println("线程1完成");
-              System.out.println("end:"+System.currentTimeMillis());
-          }
-        };
-        Thread s2 = new Thread(){
-            @Override
-            public void run(){
-                System.out.println("线程2启动...，执行1.5s");
-                try {
-                    Thread.sleep(1500);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                System.out.println("线程2完成...");
-                latch.countDown();
-
+        Thread s1 = new Thread(() -> {
+            System.out.println("线程1启动...，等待线程2、3完成后通知其");
+            try {
+                latch.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-        };
-        Thread s3 = new Thread(){
-            @Override
-            public void run(){
-                System.out.println("线程3启动...，执行2s");
-                try {
-                    Thread.sleep(2000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                System.out.println("线程3完成...");
-                latch.countDown();
-
+            System.out.println("线程1完成");
+            System.out.println("end:"+System.currentTimeMillis());
+        });
+        Thread s2 = new Thread(() -> {
+            System.out.println("线程2启动...，执行1.5s");
+            try {
+                Thread.sleep(1500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-        };
+            System.out.println("线程2完成...");
+            latch.countDown();
+
+        });
+        Thread s3 = new Thread(() -> {
+            System.out.println("线程3启动...，执行2s");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("线程3完成...");
+            latch.countDown();
+
+        });
 
         System.out.println("begin:"+System.currentTimeMillis());
         s1.start();
