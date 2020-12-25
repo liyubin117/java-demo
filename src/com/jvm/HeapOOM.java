d565983ba3ab129e44279002fca983c70a7978aa
Yubin Li <lixin58688@163.com>
2020-12-25 14:52:45 +0800

    更新

--- src/com/jvm/HeapOOM.java
+++ src/com/jvm/HeapOOM.java
@@ -9,7 +9,7 @@ import java.util.List;
  */
 public class HeapOOM {
 	static class OOMObject{
-		String s="liyubin";
+		String s="rick";
 	}
 	//vm参数：-verbose:gc -Xms10M -Xmx10M -Xmn5M -XX:+PrintGCDetails -XX:SurvivorRatio=8 -XX:+UseSerialGC -XX:+HeapDumpOnOutOfMemoryError
 	public static void main(String[] args) {
