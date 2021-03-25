89a48180f3899738749593fab52fd9193392e5fd
Yubin Li <lixin58688@163.com>
2021-03-26 01:08:32 +0800

    nowcoder

---
+++ src/com/coder/LastWordLength.java
@@ -0,0 +1,23 @@
+package com.coder;
+
+import java.io.InputStream;
+
+/**
+ * 字符串最后一个单词的长度，单词以空格分开
+ */
+public class LastWordLength {
+    public static void main(String[] args) throws Exception{
+        int times = 0;
+        InputStream in = System.in;
+        char c = (char) in.read();
+        while (c != '\n') {
+            if (c == ' ') {
+                times = 0;
+            } else {
+                times += 1;
+            }
+            c = (char) in.read();
+        }
+        System.out.println(times);
+    }
+}
