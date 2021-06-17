be2f2944408c274e925eb2cdcbf8c966f071b32a
Yubin Li <lixin58688@163.com>
2021-06-17 20:04:50 +0800

    spi机制

--- src/com/coder/SubSeqMaxSum.java
+++ src/com/coder/SubSeqMaxSum.java
@@ -7,6 +7,11 @@ import java.io.InputStreamReader;
 //连续子串和
 public class SubSeqMaxSum {
     public static void main(String[] args) throws IOException {
+        /**
+         * 动态规划
+         * 状态转移方程：dp[i] = max(dp[i-1] + nums[i], nums[i])，其中dp[i]表示以索引i为结束点基准的子数组的最大值。
+         * 初始状态：dp[0]= nums[0]
+         */
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         int[] nums = {-1, 2, 3, -2, 6, -3};
         int currMaxSum = nums[0];
---
+++ src/com/spi/SpiImpl1.java
@@ -0,0 +1,7 @@
+package com.spi;
+
+public class SpiImpl1 implements SPIService{
+    public void execute() {
+        System.out.println("SpiImpl1.execute()");
+    }
+}
