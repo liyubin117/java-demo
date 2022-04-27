550c697d5f877b250bf905181f2eeabe45019765
Yubin Li <lixin58688@163.com>
2022-04-27 16:34:57 +0800

    use calcite to resolove column name

--- java-sql/src/main/java/org/rick/calcite/ResolveColumnName.java
+++ java-sql/src/main/java/org/rick/calcite/ResolveColumnName.java
@@ -2,49 +2,60 @@ package org.rick.calcite;
 
 import org.apache.calcite.config.Lex;
 import org.apache.calcite.sql.*;
+import org.apache.calcite.sql.parser.SqlParseException;
 import org.apache.calcite.sql.parser.SqlParser;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class ResolveColumnName {
     public static void main(String[] args) throws Exception {
+        System.out.println(
+                handleSql("select myudf(id,NAME) + udf1(age) as newf,name,age FROM stu where age<20"));
+    }
+
+    public static Set<String> handleSql(String sql) throws SqlParseException {
+        Set<String> result = new HashSet<>();
         SqlParser.Config config = SqlParser.configBuilder()
                 .setLex(Lex.JAVA).build();
         SqlParser sqlParser = SqlParser
-                .create("select myudf(id,NAME) + udf1(age) as newf,name,age FROM stu where age<20", config);
+                .create(sql, config);
         SqlSelect sqlNode = (SqlSelect) sqlParser.parseStmt();
         List<SqlNode> list = sqlNode.getSelectList().getList();
-        System.out.println(list);
-        list.forEach(e->{
-            handlerField(e);
-        });
+        for (SqlNode field : list) {
+            Set<String> cols = new HashSet<>();
+            handleField(field, cols);
+            result.addAll(cols);
+        }
+        return result;
     }
 
-
-    private static void handlerField(SqlNode field) {
+    public static void handleField(SqlNode field, Set<String> cols) {
         SqlKind kind = field.getKind();
         switch (kind) {
             case AS:
                 SqlNode[] operands_as = ((SqlBasicCall) field).operands;
                 SqlNode left_as = operands_as[0];
-                handlerField(left_as);
+                handleField(left_as, cols);
                 break;
             case IDENTIFIER:
                 //表示当前为子节点
                 SqlIdentifier sqlIdentifier = (SqlIdentifier) field;
-                System.out.println("===field===" + sqlIdentifier.toString());
+                System.out.println(sqlIdentifier);
+                cols.add(sqlIdentifier.toString());
                 break;
             default:
                 if (field instanceof SqlBasicCall) {
                     SqlNode[] nodes = ((SqlBasicCall) field).operands;
                     for (int i = 0; i < nodes.length; i++) {
-                        handlerField(nodes[i]);
+                        handleField(nodes[i], cols);
                     }
                 }
                 if (field instanceof SqlNodeList) {
-                    ((SqlNodeList) field).getList().forEach(node -> {
-                        handlerField(node);
-                    });
+                    for (SqlNode node : ((SqlNodeList) field).getList()) {
+                        handleField(node, cols);
+                    }
                 }
                 break;
         }
