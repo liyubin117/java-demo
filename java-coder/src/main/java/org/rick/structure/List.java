e2d40c75f40e14c416e0c1f488aa0db4d42dda95
Yubin Li <lixin58688@163.com>
2022-07-27 11:36:14 +0800

    structure: linked list

---
+++ java-coder/src/main/java/org/rick/structure/List.java
@@ -0,0 +1,17 @@
+package org.rick.structure;
+
+public interface List<E> {
+
+    boolean add(E e);
+
+    boolean addFirst(E e);
+
+    boolean addLast(E e);
+
+    boolean remove(Object o);
+
+    E get(int index);
+
+    void printLinkList();
+
+}
--- java-coder/src/test/java/org/rick/AppTest.java
+++ java-coder/src/test/java/org/rick/AppTest.java
@@ -3,6 +3,8 @@ package org.rick;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.rick.structure.LinkedList;
+import org.rick.structure.List;
 
 /**
  * Unit test for simple App.
@@ -16,5 +18,22 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void testLink() {
+        List<String> list = new LinkedList<>();
+        // 添加元素
+        list.add("a");
+        list.addFirst("b");
+        list.addLast("c");
+        // 打印列表
+        list.printLinkList();
+        // 头插元素
+        list.addFirst("d");
+        // 删除元素
+        list.remove("b");
+        // 打印列表
+        list.printLinkList();
     }
 }
