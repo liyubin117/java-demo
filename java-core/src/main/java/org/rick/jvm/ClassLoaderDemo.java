3add776527e757a41da2f70d61fb2c6fdf609b48
Yubin Li <lixin58688@163.com>
2023-11-03 18:12:31 +0800

    update classloader

--- java-core/src/main/java/org/rick/jvm/ClassLoaderDemo.java
+++ java-core/src/main/java/org/rick/jvm/ClassLoaderDemo.java
@@ -9,33 +9,14 @@ import java.io.InputStream;
 public class ClassLoaderDemo {
     @Test
     public void testEquality() throws ClassNotFoundException, IllegalAccessException, InstantiationException {
-        ClassLoader loader = new ClassLoader() {
-            @Override
-            public Class<?> loadClass(String name) throws ClassNotFoundException {
-                try {
-                    System.out.println(System.getProperty("user.dir") + "\t" + this.getClass().getClassLoader());
-                    String fileName = "target/classes/" + name.replaceAll("\\.","/")+".class";
-                    System.out.println(fileName);
-                    InputStream is = new FileInputStream(fileName);
-                    if(is == null){
-                        return super.loadClass(name);
-                    }
-                    byte[] b = new byte[is.available()];
-                    is.read(b);
-                    return defineClass(name, b, 0, b.length);
-                } catch (IOException e) {
-                    System.out.println("IOException catched");
-                    return super.loadClass(name);
-//                    e.printStackTrace();
-                }
-//                return super.loadClass(name);
-            }
-        };
-
-        Object o = loader.loadClass("org.rick.jvm.ClassLoaderDemo").newInstance();
+        Object o = new MyClassLoader().loadClass("org.rick.jvm.ClassLoaderDemo").newInstance();
         System.out.println(o.getClass().getClassLoader());
         System.out.println(o.getClass().getClassLoader().getParent());
         System.out.println(o instanceof ClassLoaderDemo);
+
+        Object o2 = new MySubClassLoader().loadClass("org.rick.jvm.ClassLoaderDemo").newInstance();
+        System.out.println(o2.getClass().getClassLoader());
+        System.out.println(o2.getClass().getClassLoader().getParent());
     }
 
     @Test
@@ -51,5 +32,38 @@ public class ClassLoaderDemo {
         ClassLoader bootLoader = extLoader.getParent();
         System.out.println("启动类加载器：" + bootLoader);
         System.out.println("启动类加载器的加载路径：" + System.getProperty("sun.boot.class.path"));
+    }
+}
+
+class MyClassLoader extends ClassLoader {
+    @Override
+    public Class<?> loadClass(String name) throws ClassNotFoundException {
+        try {
+            System.out.println(System.getProperty("user.dir") + "\t" + this.getClass().getClassLoader());
+            String fileName = "target/classes/" + name.replaceAll("\\.","/")+".class";
+            System.out.println(fileName);
+            InputStream is = new FileInputStream(fileName);
+            if(is == null){
+                return super.loadClass(name);
+            }
+            byte[] b = new byte[is.available()];
+            is.read(b);
+            return defineClass(name, b, 0, b.length);
+        } catch (IOException e) {
+            System.out.println("IOException catched");
+            return super.loadClass(name);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "this is my class loader";
+    }
+}
+
+class MySubClassLoader extends MyClassLoader {
+    @Override
+    public String toString() {
+        return "this is my sub class loader";
     }
 }
