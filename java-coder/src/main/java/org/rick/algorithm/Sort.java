913e739bcac4e9815906f3b7ef38e8e6a3d265c2
Yubin Li <lixin58688@163.com>
2024-06-04 01:42:23 +0800

    add docker module, add prometheus docker-compose

--- java-coder/src/main/java/org/rick/algorithm/Sort.java
+++ java-coder/src/main/java/org/rick/algorithm/Sort.java
@@ -6,17 +6,16 @@ public class Sort {
     @Test
     public void testBubbleSort() {
         int[] array={10,8,20,15,21};
-        int[] arr=Sort.bubbleSort(array);
-        for (int i:arr)
+//        bubbleSort(array);
+        selectionSort(array);
+        for (int i:array)
             System.out.print(i+" ");
     }
 
 	/**
      * 冒泡排序
      */
-    public static int[] bubbleSort(int[] array) {
-        if (array.length == 0)
-            return array;
+    public static void bubbleSort(int[] array) {
         for (int i = 0; i < array.length; i++)
             for (int j = 0; j < array.length - 1 - i; j++)
                 if (array[j + 1] < array[j]) {
@@ -24,6 +23,23 @@ public class Sort {
                     array[j + 1] = array[j];
                     array[j] = temp;
                 }
-        return array;
+    }
+
+    /* 选择排序 */
+    void selectionSort(int[] nums) {
+        int n = nums.length;
+        // 外循环：未排序区间为 [i, n-1]
+        for (int i = 0; i < n - 1; i++) {
+            // 内循环：找到未排序区间内的最小元素
+            int k = i;
+            for (int j = i + 1; j < n; j++) {
+                if (nums[j] < nums[k])
+                    k = j; // 记录最小元素的索引
+            }
+            // 将该最小元素与未排序区间的首个元素交换
+            int temp = nums[i];
+            nums[i] = nums[k];
+            nums[k] = temp;
+        }
     }
 }
